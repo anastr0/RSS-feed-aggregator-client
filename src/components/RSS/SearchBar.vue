6b687d8217284b17c1b3d9971773a2c536949011
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-03-13 11:39:54 +0530

    Searchbar to input RSS-link

---
+++ src/components/RSS/SearchBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-container class="search-bar" fluid> 
+    <b-row>     
+        <b-col sm="10">
+            <b-form-input v-model="feedUrl" :id="url" placeholder="Enter RSS feed link"></b-form-input>
+        </b-col>
+        <b-col sm="2">
+            <b-button class="search-button" variant="dark" v-on:click="consoleURL">Get feed</b-button>
+        </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import fetchRSSFeed from "../../utils/fetchRSS"
+
+  export default {
+    name: "SearchBar",
+    data() {
+      return {
+        feedUrl: '',
+        feed: []
+      }
+    },
+    methods: {
+        consoleURL () {
+            alert(this.feedUrl);
+            // this.feed = fetchRSSFeed();
+        }
+    }
+  }
+</script>
+
+<style scoped> 
+.search-bar {
+    padding: 50px;
+}
+.search-button {
+    width: inherit;
+}
+</style>
