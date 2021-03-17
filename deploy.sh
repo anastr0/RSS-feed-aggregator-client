4640c98a3a391a133abb5d338100c0aa298465cf
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-03-17 08:43:45 +0530

    Rename to correct name vue.config.js

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,7 @@ set -e
 npm run build
 # navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
