080a2e8f37598c75697865d48b3d693a017d2a4d
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-03-17 08:15:18 +0530

    GetRSS parent component

--- src/components/RSS/GetRSSFeed.vue
+++ src/components/RSS/GetRSSFeed.vue
@@ -1,23 +1,49 @@
 <template>
   <div>
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <SearchBar />
-    <SearchResults />
+    <SearchBar v-on:get-feed="getFeed" />
+    <div>
+    <FeedTable v-if="queryDone" v-bind:feed="feed" />
+    </div>
+    <div>
+    <DailyFeedTable />
+    </div>
   </div>
 </template>
 
 <script>
-import SearchBar from './SearchBar.vue'
-import SearchResults from './SearchResults.vue'
+import SearchBar from "./SearchBar.vue";
+import FeedTable from "./FeedTable.vue";
+import DailyFeedTable from "./DailyFeedTable.vue";
+import { fetchRSSFeed } from "../../utils/fetchers";
 
 export default {
-  name: 'GetRSSFeed',
+  name: "GetRSSFeed",
   components: {
     SearchBar,
-    SearchResults,
+    FeedTable,
+    DailyFeedTable
   },
-}
+  data() {
+    return {
+      feed: Array,
+      // resultsBackup is needed for frontend filtering of results by isactive checkbox
+      queryDone: false,
+    };
+  },
+  methods: {
+    getFeed: function(feedURL) {
+      console.log(feedURL);
+      fetchRSSFeed(feedURL).then((data) => {
+        // console.log(data);
+        this.queryDone = true;
+        this.feed = data.RSSFeedList
+        // console.log(this.feed)
+      });
+      
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
